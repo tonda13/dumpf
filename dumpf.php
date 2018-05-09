6c56aafab9a6d213eebf5a8a1dd2a35964ec5521
Neumann <neumann@tonda.info>
2018-05-09 19:14:35 +0200

    Update library  - add TAB constant  - add echo;exit;  - add ob_end_clean;var_dump;exit;  - add UTF8 print support

--- dumpf.php
+++ dumpf.php
@@ -3,11 +3,15 @@
  * Rich debugger function
  * Author: Antonín Neumann
  * Version: 0.9.3 (10-2015)
- * 
+ *
  * Change log:
  *  - added _e() method alias for echo();
- * 
+ *
  */
+
+if (!defined('TAB')) {
+    define('TAB', "\t");
+}
 
 /**
  * Alias for echo() function
@@ -17,12 +21,31 @@ function _e($str){
 }
 
 /**
+ * Alias for echo() function and call exit() at last
+ */
+function _ex($str){
+    echo($str);
+    exit;
+}
+
+
+
+/**
  * Call var_dump function wrapped with <pre> tag
- */    
+ */
 function pre_dump(){
     echo('<pre>');
     call_user_func_array("var_dump", func_get_args());
     echo('</pre>');
+}
+
+/**
+ * Alias for dumpf() with clean output buffer and call exit() at last
+ */
+function _dcx() {
+    while(ob_end_clean());
+    call_user_func_array("dumpf", func_get_args());
+    exit();
 }
 
 /**
@@ -41,6 +64,17 @@ function _d(){
 }
 
 /**
+ * Dumpf to strign
+ */
+function _sd() {
+    ob_start();
+    call_user_func_array("dumpf", func_get_args());
+    $output = ob_get_contents();
+    ob_end_clean();
+    return $output;
+}
+
+/**
  * Alias for dumpf_pairs()
  */
 function _dp(){
@@ -52,6 +86,7 @@ function _dp(){
  */
 function _dpx(){
     call_user_func_array("dumpf_pairs", func_get_args());
+    exit();
 }
 
 /**
@@ -59,6 +94,7 @@ function _dpx(){
  */
 function dumpfx_pairs(){
     call_user_func_array("dumpf_pairs", func_get_args());
+    exit();
 }
 
 /**
@@ -69,13 +105,21 @@ function dumpfx(){
     exit();
 }
 
+function set_utf8_encoding_header() {
+    // set only if not set yet
+    if (!headers_sent()) {
+        header('Content-Type: text/html; charset=utf-8');
+    }
+}
+
 /**
  * Print rich value of variable(s)
  * Prvni parametr je jmeno a druhy promenna
  * Vypise jmeno = promenna
  * @param mixed $expression (one or more)
  */
-function dumpf_pairs(){
+function dumpf_pairs() {
+    set_utf8_encoding_header();
     if(func_num_args() <= 0) {
         return;
     }
@@ -101,28 +145,32 @@ function dumpf_pairs(){
  * Print rich value of variable(s)
  * @param mixed $expression (one or more)
  */
-function dumpf(){
+function dumpf() {
+    set_utf8_encoding_header();
     if(func_num_args() <= 0) {
         return;
     }
-    echo("<pre>");
+    echo("<pre style='font-size: 110%; position: relative; z-index: 999;'>");
     call_user_func_array("dumpf_var", func_get_args());
     echo("</pre>");
 }
 
 function dumpf_var(){
     foreach (func_get_args() as $arg) {
-        if(is_array($arg)){
+        if(is_array($arg)) {
             dumpf_array($arg, 1);
-        }else if(is_object($arg)){
+        }
+        else if(is_object($arg)) {
             dumpf_object($arg, 1);
-        }else {
-            if(is_null($arg)){
+        }
+        else {
+            if(is_null($arg)) {
                 echo("<strong>NULL</strong>\n");
             }
             else if(is_string($arg)){
                 echo(gettype($arg)." (".strlen($arg)."): <strong>".(empty($arg)?"EMPTY":$arg)."</strong>\n");
-            }else {
+            }
+            else {
                 echo(gettype($arg).": <strong>".dumpf_str_bool($arg)."</strong>\n");
             }
         }
@@ -136,7 +184,7 @@ function dumpf_var(){
  * @param int $level Level of nesting
  * @param string $key (optional) Jmeno promenne obsahujici zanorene pole
  */
-function dumpf_array($arr, $level, $key = NULL){
+function dumpf_array($arr, $level, $key = NULL) {
     $tab = "";
     for($i = 1; $i < $level; $i++){
         $tab .= "\t";
@@ -150,14 +198,16 @@ function dumpf_array($arr, $level, $key = NULL){
     if($arr_count > 0){
         echo("\n");
         foreach($arr as $key => $item){
-            if(is_array($item)){
+            if(is_array($item)) {
                 dumpf_array($item, $level+1, $key);
-            }else if(is_object($item)){
+            }
+            else if(is_object($item)){
                 dumpf_object($item, $level+1, $key);
-            }else {
+            }
+            else {
                 echo($tab."\t[<em>".$key."</em>] => "
                         . "<strong>".dumpf_str_bool($item)."</strong> "
-                        . "(".gettype($item).")\n");
+                        . "(<span class='dumpf variable-type'>".gettype($item)."</span>)\n");
             }
         }
         echo($tab."}\n");
@@ -174,21 +224,36 @@ function dumpf_array($arr, $level, $key = NULL){
  * @param int $level Level of nesting
  * @param string $key (optional) Jmeno promenne obsahujici zanoreny objekt
  */
-function dumpf_object($obj, $level, $key = NULL){
-    $tab = "";
+function dumpf_object($obj, $level, $key = NULL) {
+    $special_object_printers = array(
+        "Dibi\DateTime" => function($obj, $tab) {
+            echo($tab."\t<em>date: </em><strong>".$obj->format("d. m. Y H:i:s")."</strong>\n");
+            echo($tab."\t<em>timezone: </em><strong>".$obj->format("e P")."</strong>\n");
+            echo($tab."\t<em>timestap: </em><strong>".$obj->format("U")."</strong>\n");
+        }
+    );
+
+    $tab = '';
     for($i = 1; $i < $level; $i++){
-        $tab .= "\t";
+        $tab .= TAB;
     }
     echo($tab);
     if(isset($key)){
-        echo("[".$key."] => ");
+        echo('['.$key.'] => ');
     }
 
     $reflect = new ReflectionClass($obj);
     $properties = $reflect->getProperties();
 
-    echo("object: <strong>" . get_class($obj) . "</strong> {");
-    if(count($properties) > 0){
+    // blue: #016FD9, red: #D42A2A, yellow: #f1c40f
+    echo("object <strong style='color: #D42A2A;' class='dumpf object-name'>" . get_class($obj) . "</strong> {");
+
+    if(array_key_exists(get_class($obj), $special_object_printers)) {
+        echo("\n");
+        $special_object_printers[get_class($obj)]($obj, $tab);
+        echo($tab."}\n");
+    }
+    else if(count($properties) > 0){
         echo("\n");
         foreach($properties as $item){
             $var_name = $item->getName();
@@ -217,11 +282,16 @@ function dumpf_object($obj, $level, $key = NULL){
                         . "(".gettype($var_value).")\n");
             }
         }
-        echo($tab."}\n");
-    }else {
-        echo("\n");
+        echo($tab . '}' . PHP_EOL);
+    }
+    else {
+        echo(PHP_EOL);
         dumpf_array($obj, $level+1);
-        echo($tab."}\n");
+        // $methods = get_class_methods($obj);
+        // foreach ($methods as $method) {
+        //     echo($tab . TAB . $method . '()' . PHP_EOL);
+        // }
+        echo($tab.'}' . PHP_EOL);
     }
 }
 
@@ -266,14 +336,14 @@ function bp($x = NULL, $pre = false) { breakp($x, $pre); }
 function breakpoint($x = NULL, $pre = false) { breakp($x, $pre); }
 /**
  * Vytvoří v aplikaci breakpoint s číslem a výpisem stringu
- * 
+ *
  * @staticvar int $count pořadové číslo breakpointu
  * @param mixed $x Obsah zprávy, která se vypíše
  * @param bool $pre Pokud je TRUE vypíše se celý breakpoint mezi tagy [pre]
  */
 function breakp($x = NULL, $pre = false){
     static $count = 1;
-    
+
     $opt = array(
         1 => "Asgard",
         "Goa'uld",
@@ -287,14 +357,14 @@ function breakp($x = NULL, $pre = false){
         "Athosian",
         "Unas"
     );
-    
+
     if(is_numeric($x)){
         intval($x);
         $x = ($x % count($opt)) + 1;
     }else if(is_string($x)){
         $x = (strlen($x) % count($opt)) + 1;
     }
-    
+
     $opt[$x] == NULL ? $str = "HERE" : $str = $opt[$x];
     if($pre){
         printf("<pre>[BP-%d]: %s\n</pre>",$count++, $str);
